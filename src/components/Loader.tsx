a36e3f19b523abcde7c0f9810063b8b373635b4d
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-09-30 11:50:48 +0530

    loader bug solved

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ import { CircularProgress } from "@mui/material";
 
 const Loader = () => {
   return (
-    <div className="fixed top-0 flex items-center justify-center h-screen w-full bg-slate-800/[0.05]">
+    <div className="fixed top-0 letf-0 right-0 flex items-center justify-center h-screen w-full bg-slate-800/[0.05]">
       <CircularProgress />
     </div>
   );
