b6037b1e752820f92143c4d9bc0f30730bef5742
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-10-13 21:29:20 +0530

    scollbar added in history

--- src/module/Analyze/Analyze.tsx
+++ src/module/Analyze/Analyze.tsx
@@ -5,6 +5,7 @@ import Gauge from "@/components/Gauge/Gauge";
 import React, { useEffect, useState } from "react";
 import AnalyzeInput from "./AnalyzeInput";
 import { SessionReviewType } from "@/utils/types";
+import History from "./History";
 
 const Analyze = () => {
   const [sessionStorageReviews, setSessionStorageReviews] = useState<
@@ -24,7 +25,7 @@ const Analyze = () => {
   return (
     <>
       {isApiCalling && <Loader />}
-      <div>
+      <div className="grid grid-cols-1 gap-4">
         <div className="grid grid-cols-2 gap-4">
           <AnalyzeInput
             sentence={sentence}
@@ -36,38 +37,7 @@ const Analyze = () => {
           />
           <Gauge value={percentage} sentimentType={sentimentType} />
         </div>
-        {sessionStorageReviews.length !== 0 && (
-          <div className="mt-3 p-2">
-            <p className="text-2xl font-semibold">
-              <u>Review Analysis</u>
-            </p>
-            {sessionStorageReviews.map((item, index) => (
-              <div
-                key={`local-storage-review-index:${index}`}
-                className="flex justify-between items-center w-full p-3 mt-2 cursor-pointer rounded-md border bg-gray-200/70"
-              >
-                <p>{item.sentence}</p>
-                <p className="ml-2 text-white">
-                  {item.type === "Positive" && (
-                    <span className="p-2 rounded-md bg-green-500">
-                      {item.type}
-                    </span>
-                  )}
-                  {item.type === "Negative" && (
-                    <span className="p-2 rounded-md bg-red-500">
-                      {item.type}
-                    </span>
-                  )}
-                  {item.type === "Neutral" && (
-                    <span className="p-2 rounded-md bg-blue-500">
-                      {item.type}
-                    </span>
-                  )}
-                </p>
-              </div>
-            ))}
-          </div>
-        )}
+        <History sessionStorageReviews={sessionStorageReviews} />
       </div>
     </>
   );
