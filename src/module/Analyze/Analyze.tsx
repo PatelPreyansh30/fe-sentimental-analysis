3fac85c447bc14d410927caa1c822c10c699093f
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-10-13 11:15:46 +0530

    bug fix and code refactor

--- src/module/Analyze/Analyze.tsx
+++ src/module/Analyze/Analyze.tsx
@@ -2,109 +2,44 @@
 
 import Loader from "@/components/Loader";
 import Gauge from "@/components/Gauge/Gauge";
-import { ApiConstant } from "@/constant/applicationConstant";
-import appClient from "@/network/appClient";
-import { joiUtils } from "@/utils/joiValidation";
-import { ToastErrorMessage } from "@/utils/toastifyAlerts";
-import { TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import AnalyzeInput from "./AnalyzeInput";
+import { SessionReviewType } from "@/utils/types";
 
 const Analyze = () => {
   const [sessionStorageReviews, setSessionStorageReviews] = useState<
-    { sentence: string; type: string }[]
+    SessionReviewType[]
   >([]);
   const [sentence, setSentence] = useState<string>("");
   const [isApiCalling, setIsApiCalling] = useState(false);
-  const [isButtonClicked, setIsButtonClicked] = useState(false);
-  const [percentage, setPercentage] = useState(0)
-  const [sentimentType, setSentimentType] = useState('Postive')
+  const [percentage, setPercentage] = useState(0);
+  const [sentimentType, setSentimentType] = useState("Postive");
 
   useEffect(() => {
     let reviews = sessionStorage.getItem("review");
-    let reviewsList: { sentence: string; type: string }[] = reviews
-      ? JSON.parse(reviews)
-      : [];
+    let reviewsList: SessionReviewType[] = reviews ? JSON.parse(reviews) : [];
     setSessionStorageReviews(reviewsList);
-  }, []);
+  }, [sessionStorageReviews]);
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsButtonClicked(false);
-    setIsApiCalling(false);
-    const { value } = e.currentTarget;
-    setSentence(value);
-  };
-
-  const handleOnClick = () => {
-    setIsButtonClicked(true);
-    const result = joiUtils.validateReviewData(sentence);
-    if (result.status) {
-      getReviewAnalysis();
-    } else {
-      ToastErrorMessage(result.message);
-    }
-  };
-  const getReviewAnalysis = async () => {
-    setIsApiCalling(true);
-    try {
-      const res = await appClient.post(ApiConstant.GET_REVIEW_RESULT, {
-        sentence: sentence,
-      });
-      const scaledProbabilitRounded = res.data.result.scaled_probability
-      setPercentage(Math.round(scaledProbabilitRounded))
-      setSentimentType(res.data.result.type)
-      // Setting new review in session storage
-      let existingReview = sessionStorage.getItem("review");
-      let reviewObjectList: { sentence: string; type: string }[] =
-        existingReview ? JSON.parse(existingReview) : [];
-      reviewObjectList.push({
-        sentence: sentence,
-        type: res.data.result.type,
-      });
-      let updatedReviewList = JSON.stringify(reviewObjectList);
-      sessionStorage.setItem("review", updatedReviewList);
-
-      // Setting new review in state
-      setSessionStorageReviews((state) => [
-        ...state,
-        { sentence: sentence, type: res.data.result.type },
-      ]);
-
-      setIsApiCalling(false);
-    } catch {
-      setIsApiCalling(false);
-      setIsButtonClicked(false);
-    }
-  };
-
-  const textColor = '#AAA';
   return (
     <>
       {isApiCalling && <Loader />}
       <div>
-        <TextField
-          value={sentence}
-          onChange={handleOnChange}
-          name="sentence"
-          id="outlined-textarea"
-          label="Enter your review"
-          multiline
-          fullWidth
+        <AnalyzeInput
+          sentence={sentence}
+          setSentence={setSentence}
+          setIsApiCalling={setIsApiCalling}
+          setPercentage={setPercentage}
+          setSentimentType={setSentimentType}
+          setSessionStorageReviews={setSessionStorageReviews}
         />
-        <button
-          onClick={handleOnClick}
-          className={`mt-3 px-6 py-2 rounded-full text-white font-semibold bg-green-500 ${isButtonClicked ? "cursor-not-allowed" : "hover:bg-green-600"
-            }`}
-          disabled={isButtonClicked}
-        >
-          Analyze
-        </button>
         <Gauge value={percentage} sentimentType={sentimentType} />
         {sessionStorageReviews.length !== 0 && (
           <div className="mt-3 p-2">
             <p className="text-2xl font-semibold">
               <u>Review Analysis</u>
             </p>
-            {sessionStorageReviews.reverse().map((item, index) => (
+            {sessionStorageReviews.map((item, index) => (
               <div
                 key={`local-storage-review-index:${index}`}
                 className="flex justify-between items-center w-full p-3 mt-2 cursor-pointer rounded-md border bg-gray-200/70"
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,13 +1,18 @@
 // Types decraled here
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+
+export interface SessionReviewType {
+  sentence: string;
+  type: string;
+}
 
 export interface GaugePropsTypes {
-    value: number;
-    sentimentType: string
+  value: number;
+  sentimentType: string;
 }
 
 export interface ReviewsProviderProps {
-    valueStart: number;
-    valueEnd: number;
-    children: (value: number) => ReactNode;
-  }
+  valueStart: number;
+  valueEnd: number;
+  children: (value: number) => ReactNode;
+}
